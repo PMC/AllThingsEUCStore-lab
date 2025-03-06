3640e2edf272cca41de6c7cab205c1047c4b47f4
Pierre Chateau <pierre@chateau.se>
2025-03-06 13:44:33 +0100

    Updated CategoryController

---
+++ StoreApi/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApi.Data;
+using StoreApi.Entities;
+
+namespace StoreApi.Controllers
+{
+    [Route("StoreApi/[controller]")]
+    [ApiController]
+    public class CategoryController(SqliteDbContext context) : ControllerBase
+    {
+        private readonly SqliteDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetCategories()
+        {
+            return Ok(await _context.Categories
+                .Include(c => c.Products)
+                .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategoryById(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> AddCategory(Category newCategory)
+        {
+
+            _context.Categories.Add(newCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategoryById), new { id = newCategory.CategoryId }, newCategory);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, Category updatedCategory)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            category.CategoryName = updatedCategory.CategoryName;
+            category.Description = updatedCategory.Description;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+
+            var categoryToBeDeleted = await _context.Categories.FindAsync(id);
+            if (categoryToBeDeleted is null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categoryToBeDeleted);
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
