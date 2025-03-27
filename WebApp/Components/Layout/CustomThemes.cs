6635160321241a925e093e9fea81ef4f00fb41a0
Pierre Chateau <pierre@chateau.se>
2025-03-27 08:47:09 +0100

    created static themes

--- WebApp/Components/Layout/CustomThemes.cs
+++ WebApp/Components/Layout/CustomThemes.cs
@@ -2,10 +2,119 @@
 
 namespace WebApp.Components.Layout;
 
-public class ThemeExample
+public class CustomThemes
 {
+    public static MudTheme LandingPageTheme()
+    {
+        var theme = new MudTheme()
+        {
+            PaletteLight = LandingPageLightPalette,
+            PaletteDark = LandingPageDarkPalette,
+            Shadows = LandingPageShadows,
+            LayoutProperties = LandingPageLayoutProperties,
+            ZIndex = new ZIndex(),
+            Typography = LandingPageTypography
+        };
+        return theme;
+    }
 
-    private readonly PaletteLight _lightPalette_AiSearchExample = new()
+    public static MudTheme DocsTheme()
+    {
+        var theme = new MudTheme()
+        {
+            PaletteLight = DocsLightPalette,
+            PaletteDark = DocsDarkPalette,
+            LayoutProperties = new LayoutProperties()
+        };
+        return theme;
+    }
+    public static MudTheme AiSearchExample()
+    {
+        var theme = new MudTheme()
+        {
+            PaletteLight = _lightPalette_AiSearchExample,
+            PaletteDark = _darkPalette_AiSearchExample,
+            LayoutProperties = new LayoutProperties()
+        };
+        return theme;
+    }
+    public static MudTheme EUCStoreDefaultTheme()
+    {
+        return new()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                AppbarBackground = "rgb(17, 24, 39)",
+                AppbarText = "rgb(255, 255, 255)",
+                Background = "#eeeeee",
+                DrawerIcon = "#d1d5db",
+                DrawerText = "#d1d5db",
+                DrawerBackground = "#1f2937",
+                Primary = "#4969ee",
+                Error = "#ff0000",
+                Secondary = "#334155",
+                Tertiary = "#f6fdca",
+                LinesInputs = "#dedede",
+                GrayLight = "#e8e8e8",
+                GrayLighter = "#f9f9f9",
+                Black = "#110e2d",
+
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = "#7e6fff",
+                Surface = "#1e1e2d",
+                Background = "#151521",
+                AppbarText = "#92929f",
+                AppbarBackground = "rgba(26,26,39,0.8)",
+                DrawerBackground = "#1a1a27",
+                ActionDefault = "#74718e",
+                ActionDisabled = "#9999994d",
+                ActionDisabledBackground = "#605f6d4d",
+                TextPrimary = "#b2b0bf",
+                TextSecondary = "#92929f",
+                TextDisabled = "#ffffff33",
+                DrawerIcon = "#92929f",
+                DrawerText = "#92929f",
+                GrayLight = "#2a2833",
+                GrayLighter = "#1e1e2d",
+                Info = "#4a86ff",
+                Success = "#3dcb6c",
+                Warning = "#ffb545",
+                Error = "#ff3f5f",
+                LinesDefault = "#33323e",
+                TableLines = "#33323e",
+                Divider = "#5c5c6a",
+                OverlayLight = "#1e1e2d80",
+                Tertiary = "#f6fdca",
+
+            },
+            LayoutProperties = new LayoutProperties()
+            {
+                DefaultBorderRadius = "8px"
+            },
+            Typography = new Typography()
+            {
+
+                Default = new DefaultTypography()
+                {
+                    FontFamily = new[] { "InterPMC", "sans-serif"
+                },
+                    FontSize = "15px"
+                },
+                H1 = new H1Typography() { FontWeight = "700" },
+                H2 = new H2Typography() { FontWeight = "700" },
+                H3 = new H3Typography() { FontWeight = "700" },
+                H4 = new H4Typography() { FontWeight = "700" },
+                H5 = new H5Typography() { FontWeight = "700" },
+                H6 = new H6Typography() { FontWeight = "700" },
+                Button = new ButtonTypography() { FontWeight = "700" }
+
+            }
+        };
+    }
+
+    private static readonly PaletteLight _lightPalette_AiSearchExample = new()
     {
         Black = "#110e2d",
         AppbarText = "#424242",
@@ -15,7 +124,7 @@ public class ThemeExample
         GrayLighter = "#f9f9f9",
     };
 
-    private readonly PaletteDark _darkPalette_AiSearchExample = new()
+    private static readonly PaletteDark _darkPalette_AiSearchExample = new()
     {
         Primary = "#7e6fff",
         Surface = "#1e1e2d",
@@ -43,32 +152,6 @@ public class ThemeExample
         Divider = "#292838",
         OverlayLight = "#1e1e2d80",
     };
-
-
-    public static MudTheme LandingPageTheme()
-    {
-        var theme = new MudTheme()
-        {
-            PaletteLight = LandingPageLightPalette,
-            PaletteDark = LandingPageDarkPalette,
-            Shadows = LandingPageShadows,
-            LayoutProperties = LandingPageLayoutProperties,
-            ZIndex = new ZIndex(),
-            Typography = LandingPageTypography
-        };
-        return theme;
-    }
-
-    public static MudTheme DocsTheme()
-    {
-        var theme = new MudTheme()
-        {
-            PaletteLight = DocsLightPalette,
-            PaletteDark = DocsDarkPalette,
-            LayoutProperties = new LayoutProperties()
-        };
-        return theme;
-    }
 
 
     private static readonly PaletteLight DocsLightPalette = new()
